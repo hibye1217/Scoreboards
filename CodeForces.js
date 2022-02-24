73d6f8a0b9fdd80854b09810513c6a72ace1fe23
NAF <46581553+hibye1217@users.noreply.github.com>
2022-02-25 00:30:19 +0900

    1-4
    Rank, User, Hacks

--- CodeForces.js
+++ CodeForces.js
@@ -47,8 +47,17 @@ function createTag(obj){
     if ('textAlign' in obj){ tag.style.textAlign = obj.textAlign; }
     if ('paddingLeft' in obj){ tag.style.paddingLeft = obj.paddingLeft; }
     if ('fontWeight' in obj){ tag.style.fontWeight = obj.fontWeight; }
+    if ('fontSize' in obj){ tag.style.fontSize = obj.fontSize; }
     return tag;
 }
+
+function numberToPMString(num){
+    if (num > 0){ return "+" + num; }
+    else if (num < 0){ return "" + num; }
+    else{ return "±0"; }
+}
+
+
 
 function ratingHTML(rating){
     const rank = ratingToRank(rating);
@@ -64,6 +73,35 @@ function ratingHTML(rating){
     }
 }
 
+function userRatingHTML(handle, rating){
+    const rank = ratingToRank(rating);
+    if (rank in rankFirstLetter){
+        return '<span class="rated-user ' + rankToClass[rank] + '">'
+             + '<span style="color: ' + rankFirstLetter[rank] + '">' + handle[0] + '</span>'
+             + handle.substr(1)
+             + '</span>'
+    }
+    else{
+        return '<span class="rated-user ' + rankToClass[rank] + '">' + handle + '</span>'
+    }
+}
+
+function deltaHTML(value){
+    if (value > 0){ return '<span style="color: #008000; font-weight: bold">' + numberToPMString(value) + '</span>'; }
+    else if (value < 0){ return '<span style="color: #808080; font-weight: bold">' + numberToPMString(value) + '</span>'; }
+    else{ return '<span style="color: #000000; font-weight: bold">' + numberToPMString(value) + '</span>'; }
+}
+
+function hackHTML(successful, unsuccessful){
+    let res = "";
+    if (successful > 0){ res += '<span class="successfulChallengeCount">+' + successful + '</span>'; }
+    if (successful > 0 && unsuccessful > 0){ res += ' : '; }
+    if (unsuccessful > 0){ res += '<span class="unsuccessfulChallengeCount">-' + unsuccessful + '</span>'; }
+    const score = 100*successful - 50*unsuccessful;
+    if (successful > 0 || unsuccessful > 0){ res += '<br>' + deltaHTML(score); }
+    return res;
+}
+
 
 
 async function CodeForcesEdit(){
@@ -75,8 +113,30 @@ async function CodeForcesEdit(){
     }
     contestID = parseInt(contestID);
 
-    const contest = await callAPI('https://codeforces.com/api/contest.standings?contestId=' + contestID);
+    const contest = await callAPI('https://codeforces.com/api/contest.standings?contestId=' + contestID + '&showUnofficial=true');
     const problemCount = contest.problems.length;
+    
+    let officialScoreboard = [], unofficialScoreboard = [];
+    contest.rows.forEach(row => {
+        if (row.party.participantType == "CONTESTANT"){
+            officialScoreboard.push( Object.assign({}, row) );
+            unofficialScoreboard.push( Object.assign({}, row) );
+        }
+        if (row.party.participantType == "OUT_OF_COMPETITION"){
+            unofficialScoreboard.push( Object.assign({}, row) );
+        }
+    });
+
+    const officialCount = officialScoreboard.length, unofficialCount = unofficialScoreboard.length;
+    let officialRank = 1, unofficialRank = 1;
+    for (let i = 0; i < officialCount; i++){
+        if (i != 0 && officialScoreboard[i-1].points != officialScoreboard[i].points){ officialRank = i+1; }
+        officialScoreboard[i].rank = officialRank;
+    }
+    for (let i = 0; i < unofficialCount; i++){
+        if (i != 0 && unofficialScoreboard[i-1].points != unofficialScoreboard[i].points){ unofficialRank = i+1; }
+        unofficialScoreboard[i].rank = unofficialRank;
+    }
 
     let userArray = [];
     {
@@ -99,7 +159,7 @@ async function CodeForcesEdit(){
         const tr = document.createElement('tr');
 
         tr.appendChild( createTag({ tag: 'th', html: 'Rank', class: 'top left', width: '4em' }) );
-        tr.appendChild( createTag({ tag: 'th', html: 'Contestant', class: 'top' }) );
+        tr.appendChild( createTag({ tag: 'th', html: 'User', class: 'top' }) );
         tr.appendChild( createTag({ tag: 'th', html: 'Score', class: 'top', width: '4em' }) );
         tr.appendChild( createTag({ tag: 'th', html: 'Hacks', class: 'top', width: '4em' }) );
         for (let problemIndex = 0; problemIndex < problemCount; problemIndex++){
@@ -127,16 +187,47 @@ async function CodeForcesEdit(){
 
         thead.appendChild(tr);
     }
+
+    const contestList = await callAPI('https://codeforces.com/api/contest.list');
+
+    let contestIndex = 0;
+    while (contestList[contestIndex].id != contestID){ contestIndex += 1; }
     
     for (let i = 0; i < userCount; i++){
         const handle = userArray[i];
         const tr = document.createElement('tr');
         const dark = (i%2 == 0) ? 'dark' : 'light';
+        console.log(handle);
+
+        let official = false, participated = false;
+        let officialData = {}, unofficialData = {};
+        officialScoreboard.forEach(row => {
+            if (row.party.members[0].handle == handle){ officialData = row; official = true; }
+        });
+        unofficialScoreboard.forEach(row => {
+            if (row.party.members[0].handle == handle){ unofficialData = row; participated = true; }
+        });
+        if (!participated){ continue; }
+
+        const history = await callAPI('https://codeforces.com/api/user.rating?handle=' + handle);
+        let pointer = history.length - 1;
+        for (let j = 0; j <= contestIndex; j++){
+            if (pointer < 0){ break; }
+            if (history[pointer].contestId == contestList[j].id){ pointer -= 1; }
+        }
+        let rating = 0;
+        if (pointer >= 0){ rating = history[pointer].newRating; }
         
-        tr.appendChild( createTag({ tag: 'td', class: 'left '+dark, html: i+'<br><small style="color:#BBBBBB">(' + i + ')</small>' }) );
-        tr.appendChild( createTag({ tag: 'td', class: 'contestant-cell '+dark, html: handle, textAlign: 'left', paddingLeft: '1em' }) );
+        if (official){
+            tr.appendChild( createTag({ tag: 'td', class: 'left '+dark, html: officialData.rank+'<br><small style="color:#BBBBBB">(' + unofficialData.rank + ')</small>' }) );
+            tr.appendChild( createTag({ tag: 'td', class: 'contestant-cell '+dark, html: userRatingHTML(handle, rating), textAlign: 'left', paddingLeft: '1em' }) );
+        }
+        else{
+            tr.appendChild( createTag({ tag: 'td', class: 'left '+dark, html: '-'+'<br><small style="color:#BBBBBB">(' + unofficialData.rank + ')</small>' }) );
+            tr.appendChild( createTag({ tag: 'td', class: 'contestant-cell '+dark, html: '<small>*</small> ' + userRatingHTML(handle, rating), textAlign: 'left', paddingLeft: '1em' }) );
+        }
         tr.appendChild( createTag({ tag: 'td', class: dark }) );
-        tr.appendChild( createTag({ tag: 'td', class: dark }) );
+        tr.appendChild( createTag({ tag: 'td', class: dark, html: hackHTML(unofficialData.successfulHackCount, unofficialData.unsuccessfulHackCount), fontSize: '9px' }) );
         for (let j = 0; j < problemCount; j++){
             tr.appendChild( createTag({ tag: 'td', class: dark }) );
         }
